a5aceef965c29c4788d079f52183657af622400e
Aayush Kumar <aayushkumarjvs@gmail.com>
2018-06-27 17:55:09 +0530

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,30 +1,38 @@
-import numpy as np
+#!/usr/bin/env python                   using shebang to call interpreter from shell
+
+# importing all the necessary libraries
+import numpy as np  
 import matplotlib.pyplot as plot
 
+
+# two layerd neural network where hidden unit and output is counted as layer's
 INPUT_NODES = 2       
 HIDDEN_NODES = 3       
 OUTPUT_NODES = 1
 
+# adding the learning rate decay
+
 MAX_ITER = 30000
-ALPHA = .3
+ALPHA = 0.3
 
 print ("XOR Neural Learning")
 
 class xor(object):
-    def __init__(self): # __init__ methood is used for instantiating constants and varialbles
+    def __init__(self): 
+# __init__ constructor method is used for initializing all methods and variables
         self.inputLayerSize = INPUT_NODES
         self.outputLayerSize = OUTPUT_NODES
         self.hiddenLayerSize = HIDDEN_NODES
 
-        #Weights (Parameters)
+# Weights (Parameters)
         self.W1 = np.random.random((self.inputLayerSize, self.hiddenLayerSize))
         self.W2 = np.random.random((self.hiddenLayerSize, self.outputLayerSize))
 
-    def forward(self, X):           			# X matrix is 4X2 matrix
-        self.z2 = np.dot(X, self.W1)			#
-        self.a2 = self.sigmoid(self.z2)			#
-        self.z3 = np.dot(self.a2, self.W2)		#
-        yHat = self.sigmoid(self.z3)			#
+    def forward(self, X):           			
+        self.z2 = np.dot(X, self.W1)			
+        self.a2 = self.sigmoid(self.z2)			
+        self.z3 = np.dot(self.a2, self.W2)		
+        yHat = self.sigmoid(self.z3)			
         return yHat
 
     def sigmoid(self, z):
@@ -52,8 +60,6 @@ class xor(object):
         return dJdW1, dJdW2
 
 
-#---------------------------------NUMERICAL GRADIENT CHECKING------------------------------------
-
 
 # Formula = numericalGradient = (f(x+epsilon) - f(x-epsilon))/(2*epsilon)
 
@@ -63,9 +69,9 @@ class xor(object):
 a = xor()
 X = np.array([[0,0],[1,0],[0,1],[1,1]])
 y = np.array([[0],[1],[1],[0]]);
-# Training Sets
+# Training Sets which we use in the method
 
-#Iterations
+#Iteration used per step count for traning datasets
 for i in range(30000):
     #yHat = a.forward(X)
     cost1 = a.costFunction(X,y)
